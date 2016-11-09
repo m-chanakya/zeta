1bfa8b33236bbdde863e9ef2c31087c751e0c649
chanakya <chanakya.malireddy@gmail.com>
2016-11-09 17:20:00 +0530

    Changed cron job back to 20min

--- zeta/games/tasks.py
+++ zeta/games/tasks.py
@@ -8,7 +8,7 @@ from games.models import *
 Runs Every 20 minutes
 from 8 a.m to 10 p.m
 '''
-@periodic_task(run_every = crontab(minute="*/2", hour="8-22"))
+@periodic_task(run_every = crontab(minute="*/20", hour="8-22"))
 def make_new_games():
 	Cards.objects.create()
 	Tiles.objects.create()
